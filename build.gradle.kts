db820cc41f3e931ead4bd729b80672128efa8ddd
BatonchikSuhoi <72466002+BatonchikSuhoi@users.noreply.github.com>
2023-12-05 01:41:49 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-// LolTop-level build file where you can add configuration options common to all sub-projects/modules.
+// LolKekTop-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     java
     // id("com.android.application") version "8.1.3" apply false
